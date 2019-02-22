e37b4b8b3367ec977d2c7965190b5895f9596326
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-22 16:12:00 +0000

    started creating markers

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,6 @@
-$.getJSON("../data/cities.json", function (data) {
-    $.each(data, function (index, value) {
-       console.log(value);
-    });
-});
 function initMap() {
     var mapCenter = { lat: 50.7436337, lng: 7.0208038 };
     var map = new google.maps.Map(document.getElementById('map'), { zoom:3, center: mapCenter });
-
+    var marker = new google.maps.Marker({position:{lat:52.3144, lng:4.9420},map:map});
     
 }
