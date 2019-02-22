74f98c318fb8409f5a56f8b94a07a825c166344f
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-22 23:04:32 +0000

    testing json

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,21 @@
+var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open("GET", "../data/cities.json");
+xhr.send();
+
+function setData(jsonData) {
+    data = jsonData;
+}
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        setData(JSON.parse(this.responseText));
+    };
+}
+
+console.log(data);
+
 function initMap() {
     var mapCenter = { lat: 50.7436337, lng: 7.0208038 };
     var map = new google.maps.Map(document.getElementById('map'), { zoom:3, center: mapCenter });
