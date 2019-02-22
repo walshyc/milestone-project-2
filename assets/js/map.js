295334b7f9fed651218d73126d36e8b18c713f0f
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-22 15:44:12 +0000

    added flags

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,8 @@
 function initMap() {
     var mapCenter = { lat: 50.7436337, lng: 7.0208038 };
-    var map = new google.maps.Map(document.getElementById('map'), { zoom:4, center: mapCenter });
+    var map = new google.maps.Map(document.getElementById('map'), { zoom:3, center: mapCenter });
+
+    $.getJSON( "../data/cities.json", function(citiesData) {
+        console.log(cities.city.name);
+    }
 }
