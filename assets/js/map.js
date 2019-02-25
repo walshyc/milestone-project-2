94c5a5c05cc48680a943523c207b22afcd21d71c
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-25 12:28:46 +0000

    updated custom marker logo

--- assets/js/map.js
+++ assets/js/map.js
@@ -19,9 +19,10 @@ console.log(data);
 
 function initMap() {
     var mapCenter = { lat: 50.7436337, lng: 7.0208038 };
+   
     var map = new google.maps.Map(document.getElementById('map'), { zoom:3, center: mapCenter });
 
     
-    var marker = new google.maps.Marker({position:{lat:52.3144, lng:4.9420},map:map});
+    var marker = new google.maps.Marker({position:{lat:52.3144, lng:4.9420},map: map, icon: "assets/img/marker-logo.png"});
     
 }
