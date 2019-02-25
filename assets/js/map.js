53646d1d947018ae28bcb41a4105112d6f090719
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-25 12:11:31 +0000

    changed map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,26 @@
+var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open("GET", "../data/cities.json");
+xhr.send();
+
+function setData(jsonData) {
+    data = jsonData;
+}
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        setData(JSON.parse(this.responseText));
+    };
+}
+
+console.log(data);
 
 
 function initMap() {
     var mapCenter = { lat: 50.7436337, lng: 7.0208038 };
     var map = new google.maps.Map(document.getElementById('map'), { zoom:3, center: mapCenter });
 
-
-$.getJSON( "../data/cities.json", function( json ) {
-    console.log( "JSON Data received, name is " + json.city.name);
-});
     
     var marker = new google.maps.Marker({position:{lat:52.3144, lng:4.9420},map:map});
     
