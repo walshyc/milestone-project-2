04f5cf425e8d3a3d601fa02faed9899f8e424dde
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-26 21:59:44 +0000

    updated JSON and added infowindow to marker

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,7 @@
 function initMap() {
     var mapCenter = { lat: 50.7436337, lng: 18.4208038 };
 
+
     var map = new google.maps.Map(document.getElementById('map'), { zoom: 3, center: mapCenter });
     $.getJSON('assets/data/cities.json', function (data) {
 
@@ -8,14 +9,51 @@ function initMap() {
         $.each(data, function (key, value) {
             console.log(value.name);
             cityName = value.name;
+            stadiumName = value.stadium;
+            stadiumCapacity = value.capacity
             cityLat = value.stadiumLat;
             cityLng = value.stadiumLng;
+            countryFlag = value.flagIcon;
 
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(cityLat, cityLng),
                 icon: "assets/img/marker-logo.png",
-                map: map
+                map: map,
+                title: `${cityName}`
+            });
+
+            var infoWindowContent = `<div class="col">
+            <h3><span class="flag-icon flag-icon-${countryFlag}"></span> ${cityName}</h3>
+            <h5>${stadiumName}</h5>
+            <p>Capacity: ${stadiumCapacity}</p>
+        </div>`;
+
+            var infowindow = new google.maps.InfoWindow({});
+
+            infowindow = new google.maps.InfoWindow({
+                position: new google.maps.LatLng(cityLat, cityLng),
+                content: infoWindowContent
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
             })
+
+
+            marker.addListener('click', function () {
+                if (!marker.open) {
+
+                    infowindow.open(map, marker);
+                    marker.open = true
+                } else {
+                    infowindow.close();
+                    marker.open = false;
+                }
+            });
+
+
+
+
+
         });
 
 
