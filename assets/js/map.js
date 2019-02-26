6667ce2cde7b5031548ffb596ea454e2546ca08c
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-26 17:52:58 +0000

    created custom markers for each 12 cities

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,52 +1,59 @@
-$(document).ready(function() {
-    $.getJSON('../data/cities.json', function(data) {
-        var city = data.city;
-        console.log(city);
-    });
-});
-
-
 function initMap() {
-    var mapCenter = { lat: 50.7436337, lng: 7.0208038 };
-   
-    var map = new google.maps.Map(document.getElementById('map'), { zoom:3, center: mapCenter });
+    var mapCenter = { lat: 50.7436337, lng: 18.4208038 };
 
-    
-    var marker = new google.maps.Marker({position:{lat:52.3144, lng:4.9420},map: map, icon: "assets/img/marker-logo.png"});
-    
-}
+    var map = new google.maps.Map(document.getElementById('map'), { zoom: 3, center: mapCenter });
+    $.getJSON('assets/data/cities.json', function (data) {
 
-function displayCityInfo() {
-    $("#ams-btn").click(function (e) { 
-        e.preventDefault();
-        $("#bku-btn").hide();
-        $("#bil-btn").hide();
-        $("#buc-btn").hide();
-        $("#bud-btn").hide();
-        $("#cop-btn").hide();
-        $("#dub-btn").hide();
-        $("#gla-btn").hide();
-        $("#ldn-btn").hide();
-        $("#mun-btn").hide();
-        $("#rom-btn").hide();
-        $("#stp-btn").hide();
-        
-    });
 
-    $("#stp-btn").click(function (e) { 
-        e.preventDefault();
-        $("#bku-btn").hide();
-        $("#bil-btn").hide();
-        $("#buc-btn").hide();
-        $("#bud-btn").hide();
-        $("#cop-btn").hide();
-        $("#dub-btn").hide();
-        $("#gla-btn").hide();
-        $("#ldn-btn").hide();
-        $("#mun-btn").hide();
-        $("#rom-btn").hide();
-        $("#ams-btn").hide();
-        $("#stp-btn").siblings
-        
+        $.each(data, function (key, value) {
+            console.log(value.name);
+            cityName = value.name;
+            cityLat = value.stadiumLat;
+            cityLng = value.stadiumLng;
+
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(cityLat, cityLng),
+                icon: "assets/img/marker-logo.png",
+                map: map
+            })
+        });
+
+
     });
 }
+
+// function displayCityInfo() {
+//     $("#ams-btn").click(function (e) { 
+//         e.preventDefault();
+//         $("#bku-btn").hide();
+//         $("#bil-btn").hide();
+//         $("#buc-btn").hide();
+//         $("#bud-btn").hide();
+//         $("#cop-btn").hide();
+//         $("#dub-btn").hide();
+//         $("#gla-btn").hide();
+//         $("#ldn-btn").hide();
+//         $("#mun-btn").hide();
+//         $("#rom-btn").hide();
+//         $("#stp-btn").hide();
+
+//     });
+
+//     $("#stp-btn").click(function (e) { 
+//         e.preventDefault();
+//         $("#bku-btn").hide();
+//         $("#bil-btn").hide();
+//         $("#buc-btn").hide();
+//         $("#bud-btn").hide();
+//         $("#cop-btn").hide();
+//         $("#dub-btn").hide();
+//         $("#gla-btn").hide();
+//         $("#ldn-btn").hide();
+//         $("#mun-btn").hide();
+//         $("#rom-btn").hide();
+//         $("#ams-btn").hide();
+//         $("#stp-btn").siblings
+
+//     });
+// }
+
