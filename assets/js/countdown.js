91149693f454215d3660ded41fb1f36c269e3536
Conor Walsh <conorwalsh0703@gmail.com>
2020-07-08 12:57:26 +0100

    fixed city button spacing and reset timer to 2021

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-var euroStartDate = new Date("Jun 12, 2020 20:00:00").getTime();
+var euroStartDate = new Date("Jun 11, 2021 20:00:00").getTime();
 
 var a = setInterval(function() {
     var now = new Date().getTime();
@@ -12,7 +12,7 @@ var a = setInterval(function() {
     var minutes = Math.floor((length % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((length % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown").innerHTML =`Euro 2020 starts in `;
+    document.getElementById("countdown").innerHTML =`Euro 2020 has been postponed, it will now start in `;
     document.getElementById("days").innerHTML = `${days}<br> days`;
     document.getElementById("hours").innerHTML = `${hours}<br> hours`;
     document.getElementById("minutes").innerHTML = `${minutes}<br>minutes`;
